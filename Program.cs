a958b22088e4865cd4b9fb33f7ddf4fff419c17f
FlowyeHLS <king2015@list.ru>
2025-05-21 14:54:50 +0500

    AcademyHw

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadamy
+{
+	class Program
+	{
+		static readonly string delimiter = "\n---------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student("Pinkman", "Jessie", 23, "Chemisrty", "WW_40", 95, 66);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher("White", "Whalter", 50, "CHemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter);
+
+			Human tommy = new Human("Verccety", "Tommy", 30);
+			tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Graduate mark = new Graduate("Kalivan", "Ivan", 27, "Chemistry", "WW_45", 95, 66, "TablicaMendeleeva", "W.Whalter");
+			mark.Info();
+			Console.WriteLine(delimiter);
+
+			Specialist alesha = new Specialist("Popovich", "Alesha", 30, "Ximik", "WW_45", 100, 100, "Ximikov", " Ximikal_Technology");
+			alesha.Info();
+			Console.WriteLine(delimiter);
+		}
+	}
+}
---
+++ Teacher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadamy
+{
+    class Teacher:Human
+    {
+        public string Speciality { get; set; }
+        public double Expirience { get; set; }
+        public Teacher
+            (
+            string lastName, string firstName, int age,
+            string speciality, double expirience
+            ):base(lastName, firstName,age)
+        {
+            Speciality = speciality;
+            Expirience = expirience;
+            Console.WriteLine($"TConstructor: {this.GetHashCode()}");
+        }
+        ~Teacher()
+        {
+			Console.WriteLine($"TDestructor: {this.GetHashCode()}");
+		}
+        public void Info()
+        {
+            base.Info();
+			Console.WriteLine($"{Speciality} {Expirience}");
+        }
+
+
+    }
+}
